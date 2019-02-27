025ca6d98773797a00937c82f8064cb7ed4832b5
LassaadBenRejebSclabe <47390338+LassaadBenRejebSclabe@users.noreply.github.com>
2019-02-27 16:45:15 +0100

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Hook extends BaseUtil{
 
-
+public  WebDriver driver;
     private BaseUtil base;
 
     public Hook(BaseUtil base) {
@@ -36,7 +36,7 @@ System.out.println("22222222");
         base.Driver = new ChromeDriver();
         System.out.println("44444444444");
         
-        public  WebDriver driver;
+        
         System.out.println("55555");
         System.setProperty("webdriver.chrome.driver", "C:/Libs/chromedriver.exe);
 System.out.println("66666");
