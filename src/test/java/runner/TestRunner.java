05d11946933899e3dd63b23224fcd32b35505916
LassaadBenRejebSclabe <47390338+LassaadBenRejebSclabe@users.noreply.github.com>
2019-03-13 11:21:15 +0100

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -12,5 +12,9 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(features = {"src/test/java/features"} , format = {"json:target/cucumber.json","html:target/site/cucumber-pretty"},
         glue = "steps")
 public class TestRunner extends AbstractTestNGCucumberTests{
+        System.setProperty("webdriver.chrome.driver", "C:/Libs/chromedriver.exe");
+        System.out.println("3333333333333");
+        base.Driver = new ChromeDriver();
+        System.out.println("44444444444");
 
 }
